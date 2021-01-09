18bab55c139d3afb48a0b9eacd8c43e4e1856979
buyeolim <buyeolim@gmail.com>
2021-01-09 23:47:09 +0900

    BOJ 문제 풀이 업데이트

---
+++ BOJ/python3/19-S Sogang ACM-ICPC Team Application (Bronze)/2675.py
@@ -0,0 +1,17 @@
+'''
+문자열 반복
+https://www.acmicpc.net/problem/2675
+'''
+import sys
+sys.stdin = open("input.txt", "r")
+
+T = int(input()) # 테스트 케이스
+for _ in range(T):
+    R, S = input().split() # 반복횟수, 문자열
+    R = int(R)
+
+    P = '' # 새 문자열
+    for s in S:
+        P += s * R
+
+    print(P)
