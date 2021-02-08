5f399246c4a754db80e4bb11c2d46b2a4a35a95e
buyeolim <buyeolim@gmail.com>
2021-02-08 22:58:40 +0900

    BOJ 문제풀이 업데이트

--- BOJ/python3/19-S Sogang ACM-ICPC Team Application (Silver)/16433.py
+++ BOJ/python3/19-S Sogang ACM-ICPC Team Application (Silver)/16433.py
@@ -2,11 +2,6 @@
 주디와 당근농장
 https://www.acmicpc.net/problem/16433
 '''
-import sys
-import time
-start = time.time()  # 시작 시간 저장
-sys.stdin = open("input.txt", "r")  # input.txt 파일로 입력
-
 N, R, C = map(int, input().split())  # 한 변의 길이, 당근 위치
 
 # (1, 1)에서의 당근 플래그
@@ -26,6 +21,3 @@ for r in range(1, N+1):
     print()
     if c % 2 == 0:
         carrot = (carrot+1) % 2
-
-
-print("time :", time.time() - start)  # 현재시각 - 시작시간 = 실행 시간
