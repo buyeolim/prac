1ddb9f19efec9a42c5c5e6dad4b95b7fa0aca4d9
buyeolim <buyeolim@gmail.com>
2021-05-04 17:13:04 +0900

    풀이 수정-programmers-JavaScript

--- programmers/javascript/42576.js
+++ programmers/javascript/42576.js
@@ -3,16 +3,35 @@
 https://programmers.co.kr/learn/courses/30/lessons/42576
  */
 function solution(participant, completion) {
-  let answer = participant.slice();
+  let answer = "";
 
-  for (let person of completion) {
-    if (participant.includes(person)) {
-      const idx = answer.indexOf(person);
-      answer.splice(idx, 1);
+  // 참가자 Map에 저장, O(n)
+  const hash = new Map();
+  participant.forEach((person) => {
+    if (hash.has(person)) {
+      let cnt = hash.get(person);
+      hash.set(person, cnt + 1);
+    } else {
+      hash.set(person, 1);
+    }
+  });
+
+  // 완주자 Map에서 제거, O(n)
+  completion.forEach((person) => {
+    if (hash.has(person)) {
+      let cnt = hash.get(person);
+      hash.set(person, cnt - 1);
+    }
+  });
+
+  // 제거되지 않은 참가자 탐색 후 출력, O(n)
+  for (let [person, cnt] of hash) {
+    if (cnt !== 0) {
+      answer = person;
     }
   }
 
-  return answer[0];
+  return answer;
 }
 
 participant = ["mislav", "stanko", "mislav", "ana"];
@@ -22,6 +41,6 @@ console.log(solution(participant, completion));
 
 /*
  * 정확성: o / o / o / o / o
- * 효율성: x / x / x / x / x
- * 합계: 50.0 / 100.0
+ * 효율성: o / o / o / o / o
+ * 합계: 100.0 / 100.0
  */
