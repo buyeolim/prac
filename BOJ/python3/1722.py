6c8a47e50deea604e713eeb1598587546213c95f
buyeolim <buyeolim@gmail.com>
2021-11-18 21:03:43 +0900

    Modify success file from failure file(boj-1722)

--- BOJ/python3/1722.py
+++ BOJ/python3/1722.py
@@ -2,7 +2,6 @@
 순열의 순서
 https://www.acmicpc.net/problem/1722
 '''
-from itertools import permutations
 import sys
 import time
 start = time.time()  # 시작 시간
@@ -10,24 +9,53 @@ sys.stdin = open("input.txt", "r")  # input.txt 파일로 입력
 
 
 N = int(input())
-line = list(map(int, input().split()))
+Q, *R = list(map(int, input().split()))
 
-nums = [i for i in range(1, N+1)]
-P = list(permutations(nums, N))
+# k의 최댓값을 이용해 N으로 나눈 구간의 크기를 구한다.
+max_k = 1
+for n in range(1, N+1):
+    max_k *= n
+size = max_k // N
 
-Q = line.pop(0)
+P = []  # 순열
+nums = [i for i in range(1, N+1)]  # 순열에 들어가는 숫자
+ans = ""
 if Q == 1:
-    # k번째 순열 구하기
-    k = line.pop()
+    # 소문제 1. k번째 순열 구하기
+    k = R[0]
+    while (True):
+        idx = (k-1) // size
+        P.append(nums[idx])
+        nums.remove(nums[idx])
 
-    print(*P[k-1], sep=" ")
+        k = k % size
+        N -= 1
+        if (N == 0):
+            for num in P:
+                ans += str(num) + " "
+            break
+        else:
+            size = size // N
+
 else:
-    # 주어진 임의의 순열의 순서 구하기
-    p = tuple(line)
-    for i in range(N):
-        if P[i] == p:
-            print(i+1)
+    # 소문제 2. 주어진 임의의 순열의 순서 구하기
+    P = R
+    low_bound = 1
+    while (True):
+        idx = nums.index(P[0])
+        low_bound += idx * size
+        high_bound = low_bound + size - 1
+        nums.remove(P.pop(0))
 
-    # 현재시각 - 시작시간 = 실행 시간(sec)
+        N -= 1
+        if (low_bound == high_bound or N == 0):
+            ans = low_bound
+            break
+        else:
+            size = size // N
+
+print(ans)
+
+# 현재시각 - 시작시간 = 실행 시간(sec)
 print("--------------------")
 print(f"Elapsed Time: {round(time.time() - start, 3)}s")
