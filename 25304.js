18b723b118fdb2d6b4069e7e52b6e55bc36d2467
buyeolim <buyeolim@gmail.com>
2022-08-07 23:01:48 +0900

    Add new PS file(boj-25304)

--- 25304.js
+++ 25304.js
@@ -1,5 +1,6 @@
 /*
-제목
+영수증
+boj.kr/25304
  */
 const start = new Date().getTime();
 
@@ -7,8 +8,14 @@ const fs = require("fs");
 const input = fs.readFileSync("input.txt").toString().split("\n");
 //const input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-/* code */
-console.log("Hello!");
+const X = input[0] * 1;
+const N = input[1] * 1;
+let total = 0;
+for (let n = 0; n < N; n++) {
+  let [a, b] = input[2 + n].split(" ").map((ele) => ele * 1);
+  total += a * b;
+}
+X === total ? console.log("Yes") : console.log("No");
 
 const end = new Date().getTime();
 console.log("--------------------");
