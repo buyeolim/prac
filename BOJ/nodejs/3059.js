5ea1d41ed3ea6385d5b7fc799aa84587bee81373
buyeolim <buyeolim@gmail.com>
2023-02-16 20:17:39 +0900

    Add new PS file

---
+++ BOJ/nodejs/3059.js
@@ -0,0 +1,38 @@
+/*
+등장하지 않는 문자의 합
+boj.kr/3059
+ */
+
+const start = new Date().getTime();
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let lc = 0;
+let T;
+rl.on("line", function (line) {
+  if (lc === 0) {
+    T = 1 * line;
+  } else {
+    const S = line;
+
+    let ans = 0;
+    let check = Array.from({ length: 26 }, (v, i) => 65 + i);
+    for (let ch of S) check[ch.charCodeAt() - 65] = 0;
+
+    ans = check.reduce((cur, val) => cur + val, 0);
+    console.log(ans);
+  }
+
+  lc++;
+  if (lc > T) rl.close();
+}).on("close", function () {
+  process.exit();
+});
+
+const end = new Date().getTime();
+console.log("--------------------");
+console.log(`Elapsed Time: ${(new Date().getTime() - start) / 1000}s`);
