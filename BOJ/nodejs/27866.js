ca3cab7e0c74c48905e7d25ba1c03f54587b6583
buyeolim <buyeolim@gmail.com>
2023-03-09 20:08:58 +0900

    Add new PS file

---
+++ BOJ/nodejs/27866.js
@@ -0,0 +1,19 @@
+/*
+문자와 문자열
+boj.kr/27866
+ */
+const start = new Date().getTime();
+
+const fs = require("fs");
+const input = fs.readFileSync("input.txt").toString().split("\n");
+//const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const S = input[0];
+const i = input[1];
+
+let ans = S.charAt(i - 1);
+console.log(ans);
+
+const end = new Date().getTime();
+console.log("--------------------");
+console.log(`Elapsed Time: ${(new Date().getTime() - start) / 1000}s`);
