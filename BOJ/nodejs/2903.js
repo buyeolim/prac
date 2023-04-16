f446642b26d6532ddcc7fb053da0a79ba1afa107
buyeolim <buyeolim@gmail.com>
2023-04-16 21:26:44 +0900

    Add new PS file

---
+++ BOJ/nodejs/2903.js
@@ -0,0 +1,18 @@
+/*
+중앙 이동 알고리즘
+boj.kr/2903
+ */
+const start = new Date().getTime();
+
+const fs = require("fs");
+const input = fs.readFileSync("input.txt").toString().split("\n");
+//const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const N = 1 * input[0];
+
+const ans = (2 ** N + 1) ** 2;
+console.log(ans);
+
+const end = new Date().getTime();
+console.log("--------------------");
+console.log(`Elapsed Time: ${(new Date().getTime() - start) / 1000}s`);
