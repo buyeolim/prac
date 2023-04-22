12a823b95df4c5b7da22d53f3ffb6eb3f1b379bd
buyeolim <buyeolim@gmail.com>
2023-04-22 23:45:08 +0900

    Add new PS file

---
+++ BOJ/nodejs/15894.js
@@ -0,0 +1,18 @@
+/*
+수학은 체육과목 입니다
+boj.kr/15894
+ */
+const start = new Date().getTime();
+
+const fs = require("fs");
+const input = fs.readFileSync("input.txt").toString().split("\n");
+//const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const n = 1 * input[0];
+
+const ans = 4 * n;
+console.log(ans);
+
+const end = new Date().getTime();
+console.log("--------------------");
+console.log(`Elapsed Time: ${(new Date().getTime() - start) / 1000}s`);
