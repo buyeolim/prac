5742dd18d3471671bd54c519cd5a4cbb61e3db85
buyeolim <buyeolim@gmail.com>
2023-04-26 22:42:20 +0900

    Add new PS file

---
+++ BOJ/nodejs/27433.js
@@ -0,0 +1,21 @@
+/*
+팩토리얼 2
+boj.kr/27433
+ */
+const start = new Date().getTime();
+
+const fs = require("fs");
+const input = fs.readFileSync("input.txt").toString().split("\n");
+//const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const N = 1 * input[0];
+
+let ans = 1;
+for (let n = 1; n < N + 1; n++) {
+  ans *= n;
+}
+console.log(ans);
+
+const end = new Date().getTime();
+console.log("--------------------");
+console.log(`Elapsed Time: ${(new Date().getTime() - start) / 1000}s`);
