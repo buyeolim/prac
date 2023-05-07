8330d8d94a3c648922abf188474298852c9e1265
buyeolim <buyeolim@gmail.com>
2023-05-07 19:59:42 +0900

    Add new PS file

---
+++ BOJ/nodejs/13866.js
@@ -0,0 +1,17 @@
+/*
+팀 나누기
+boj.kr/13866
+ */
+const start = new Date().getTime();
+
+const fs = require("fs");
+const input = fs.readFileSync("input.txt").toString().split("\n");
+//const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const [A, B, C, D] = input[0].split(" ").map(Number);
+let ans = Math.abs(A + D - (B + C));
+console.log(ans);
+
+const end = new Date().getTime();
+console.log("--------------------");
+console.log(`Elapsed Time: ${(new Date().getTime() - start) / 1000}s`);
