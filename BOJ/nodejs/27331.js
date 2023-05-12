35169b4b29576ab15294f861468f9b05378575d0
buyeolim <buyeolim@gmail.com>
2023-05-12 23:38:08 +0900

    Add new PS file

---
+++ BOJ/nodejs/27331.js
@@ -0,0 +1,18 @@
+/*
+Two-digit Integer
+boj.kr/27331
+ */
+const start = new Date().getTime();
+
+const fs = require("fs");
+const input = fs.readFileSync("input.txt").toString().split("\n");
+//const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const A = input[0];
+const B = input[1];
+let ans = A + B;
+console.log(ans);
+
+const end = new Date().getTime();
+console.log("--------------------");
+console.log(`Elapsed Time: ${(new Date().getTime() - start) / 1000}s`);
