814266141c0ab9e363998e2748f2369d139a6437
buyeolim <buyeolim@gmail.com>
2023-06-06 22:34:55 +0900

    Add new PS file

---
+++ BOJ/nodejs/3004.js
@@ -0,0 +1,17 @@
+/*
+체스판 조각
+boj.kr/3004
+ */
+const start = new Date().getTime();
+
+const fs = require("fs");
+const input = fs.readFileSync("input.txt").toString().split("\n");
+//const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const N = 1 * input[0];
+const ans = (Math.ceil(N / 2) + 1) * (Math.floor(N / 2) + 1);
+console.log(ans);
+
+const end = new Date().getTime();
+console.log("--------------------");
+console.log(`Elapsed Time: ${(new Date().getTime() - start) / 1000}s`);
