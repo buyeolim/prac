0ea24ccab34be0565705548cb80d29290c2e8ec0
buyeolim <buyeolim@gmail.com>
2023-06-11 23:23:53 +0900

    Add new PS file

---
+++ BOJ/nodejs/23037.js
@@ -0,0 +1,18 @@
+/*
+5의 수난
+boj.kr/23037
+ */
+const start = new Date().getTime();
+
+const fs = require("fs");
+const input = fs.readFileSync("input.txt").toString().split("\n");
+//const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const n = input[0].split("").map(Number);
+
+const ans = n.reduce((acc, cur) => acc + cur ** 5, 0);
+console.log(ans);
+
+const end = new Date().getTime();
+console.log("--------------------");
+console.log(`Elapsed Time: ${(new Date().getTime() - start) / 1000}s`);
