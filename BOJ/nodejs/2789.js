89c622af98727e8c5879d871bed819b1496fddac
buyeolim <buyeolim@gmail.com>
2023-06-14 22:27:20 +0900

    Add new PS file

---
+++ BOJ/nodejs/2789.js
@@ -0,0 +1,17 @@
+/*
+유학 금지
+boj.kr/2789
+ */
+const start = new Date().getTime();
+
+const fs = require("fs");
+const input = fs.readFileSync("input.txt").toString().split("\n");
+//const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const word = input[0];
+const ans = word.replace(/[CAMBRIDGE]/g, "");
+console.log(ans);
+
+const end = new Date().getTime();
+console.log("--------------------");
+console.log(`Elapsed Time: ${(new Date().getTime() - start) / 1000}s`);
