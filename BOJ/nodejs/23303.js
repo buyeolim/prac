406a0065140fbac22c0005b86e2bce0f1f8b7f8b
buyeolim <buyeolim@gmail.com>
2023-06-28 21:14:06 +0900

    Add new PS file

---
+++ BOJ/nodejs/23303.js
@@ -0,0 +1,16 @@
+/*
+이 문제는 D2 입니다.
+boj.kr/23303
+ */
+const start = new Date().getTime();
+
+const fs = require("fs");
+const input = fs.readFileSync("input.txt").toString().split("\n");
+//const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const ans = input[0].match(/D2/gi) ? "D2" : "unrated";
+console.log(ans);
+
+const end = new Date().getTime();
+console.log("--------------------");
+console.log(`Elapsed Time: ${(new Date().getTime() - start) / 1000}s`);
