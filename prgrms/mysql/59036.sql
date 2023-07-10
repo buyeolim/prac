50d7901c4a4cb4bd8900aee4e17f02284d132991
buyeolim <buyeolim@gmail.com>
2023-07-10 23:32:31 +0900

    Add new PS file

---
+++ prgrms/mysql/59036.sql
@@ -0,0 +1,9 @@
+/*
+아픈 동물 찾기
+https://school.programmers.co.kr/learn/courses/30/lessons/59036
+ */
+
+SELECT animal_id, name
+    FROM animal_ins
+    WHERE intake_condition='Sick'
+    ORDER BY animal_id ASC
