6f8fccfdd38dcc42ac101fd70d4934af0bd60b38
buyeolim <buyeolim@gmail.com>
2023-08-05 23:10:15 +0900

    Add new PS file

---
+++ prgrms/mysql/59410.sql
@@ -0,0 +1,8 @@
+/*
+NULL 처리하기
+https://school.programmers.co.kr/learn/courses/30/lessons/59410
+ */
+
+SELECT animal_type, IFNULL(name, 'No name'), sex_upon_intake
+FROM animal_ins
+ORDER BY animal_id ASC;
