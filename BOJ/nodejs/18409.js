60f5177485a786ab25d0d18fa11b550696e6b745
buyeolim <buyeolim@gmail.com>
2023-09-24 19:52:19 +0900

    Add new PS file

---
+++ BOJ/nodejs/18409.js
@@ -0,0 +1,19 @@
+/*
+Counting Vowels
+boj.kr/18409
+ */
+const start = new Date().getTime();
+
+const fs = require("fs");
+const input = fs.readFileSync("input.txt").toString().split("\n");
+//const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const N = 1 * input[0];
+const S = input[1];
+
+const ans = S.match(/[aiueo]/g) ? S.match(/[aiueo]/g).length : 0;
+console.log(ans);
+
+const end = new Date().getTime();
+console.log("--------------------");
+console.log(`Elapsed Time: ${(new Date().getTime() - start) / 1000}s`);
