e1cdb24b7a416594323b68f9bbb15a487c359936
buyeolim <buyeolim@gmail.com>
2023-11-15 22:06:28 +0900

    Add new PS file(boj-3765.js)

---
+++ BOJ/nodejs/3765.js
@@ -0,0 +1,18 @@
+/*
+Celebrity jeopardy
+https://boj.kr/3765
+ */
+const start = new Date().getTime();
+
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .split("\n");
+
+const ans = input.join("\n");
+
+console.log(ans);
+
+const end = new Date().getTime();
+console.log("--------------------");
+console.log(`Elapsed Time: ${(new Date().getTime() - start) / 1000}s`);
