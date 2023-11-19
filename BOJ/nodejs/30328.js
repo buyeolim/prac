1a120643beb23c26db6ba6d9af6b941fcd4da7db
buyeolim <buyeolim@gmail.com>
2023-11-19 23:14:08 +0900

    Add new PS file(boj-30328.js)

---
+++ BOJ/nodejs/30328.js
@@ -0,0 +1,19 @@
+/*
+Java Warriors
+https://boj.kr/30328
+ */
+const start = new Date().getTime();
+
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .split("\n");
+
+const n = +input[0];
+const ans = n * 4000;
+
+console.log(ans);
+
+const end = new Date().getTime();
+console.log("--------------------");
+console.log(`Elapsed Time: ${(new Date().getTime() - start) / 1000}s`);
