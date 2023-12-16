5b5c38c647824c75e34fa79ca6c56c3ab641195c
buyeolim <buyeolim@gmail.com>
2023-12-16 23:01:45 +0900

    Add new PS file(boj-16600.js)

---
+++ BOJ/nodejs/16600.js
@@ -0,0 +1,18 @@
+/*
+Contemporary Art
+https://boj.kr/16600
+ */
+const start = new Date().getTime();
+
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .split("\n");
+
+const a = +input[0];
+const ans = (Math.sqrt(a) * 4).toFixed(6);
+console.log(ans);
+
+const end = new Date().getTime();
+console.log("--------------------");
+console.log(`Elapsed Time: ${(new Date().getTime() - start) / 1000}s`);
