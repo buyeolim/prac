1a895b0d7b5bdc6dd11dd6dbc74b82c53d7b01d0
buyeolim <buyeolim@gmail.com>
2023-12-23 23:08:10 +0900

    Add new PS file(boj-24723.js)

---
+++ BOJ/nodejs/24723.js
@@ -0,0 +1,18 @@
+/*
+녹색거탑
+https://boj.kr/24723
+ */
+const start = new Date().getTime();
+
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .split("\n");
+
+const N = +input[0];
+const ans = 2 ** N;
+console.log(ans);
+
+const end = new Date().getTime();
+console.log("--------------------");
+console.log(`Elapsed Time: ${(new Date().getTime() - start) / 1000}s`);
