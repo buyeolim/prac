d08829935815fe9ff4e5b88dbe49c9488ec52dd4
buyeolim <buyeolim@gmail.com>
2023-12-29 21:48:52 +0900

    Add new PS file(boj-5522.js)

---
+++ BOJ/nodejs/5522.js
@@ -0,0 +1,17 @@
+/*
+카드 게임
+https://boj.kr/5522
+ */
+const start = new Date().getTime();
+
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .split("\n");
+
+const ans = input.reduce((acc, cur) => acc + 1 * cur, 0);
+console.log(ans);
+
+const end = new Date().getTime();
+console.log("--------------------");
+console.log(`Elapsed Time: ${(new Date().getTime() - start) / 1000}s`);
