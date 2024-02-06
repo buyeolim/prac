ff837d612d811a46e39a3a3a12f5c13a770821b2
buyeolim <buyeolim@gmail.com>
2024-02-06 22:15:17 +0900

    Add new PS file(prgrms-120810.js)

---
+++ prgrms/javascript/120810.js
@@ -0,0 +1,15 @@
+/*
+나머지 구하기
+https://school.programmers.co.kr/learn/courses/30/lessons/120810
+ */
+
+function solution(num1, num2) {
+  const answer = num1 % num2;
+
+  return answer;
+}
+
+// const input = [3, 2];
+const input = [10, 5];
+
+console.log(solution(...input));
