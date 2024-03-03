cb3a46db4ab6c8e6e55111531e0c455c40d259b2
buyeolim <buyeolim@gmail.com>
2024-03-03 22:27:17 +0900

    Add new PS file(prgrms-120922.js)

---
+++ prgrms/javascript/120922.js
@@ -0,0 +1,16 @@
+/*
+종이 자르기
+https://school.programmers.co.kr/learn/courses/30/lessons/120922
+ */
+
+function solution(M, N) {
+  const answer = M * N - 1;
+
+  return answer;
+}
+
+// const input = [2, 2];
+// const input = [2, 5];
+const input = [1, 1];
+
+console.log(solution(...input));
