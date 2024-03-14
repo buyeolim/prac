7d16820210ea3b1d06f236fc929e1c91ca6633e8
buyeolim <buyeolim@gmail.com>
2024-03-14 21:49:14 +0900

    Add new PS file(prgrms-181845.js)

---
+++ prgrms/javascript/181845.js
@@ -0,0 +1,15 @@
+/*
+문자열로 변환
+https://school.programmers.co.kr/learn/courses/30/lessons/181845
+ */
+
+function solution(n) {
+  const answer = "" + n;
+
+  return answer;
+}
+
+const input = [123];
+// const input = [2573];
+
+console.log(solution(...input));
