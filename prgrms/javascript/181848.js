8793fe2b0a26c9e8d7d49610291ca39056b370d8
buyeolim <buyeolim@gmail.com>
2024-03-15 22:07:18 +0900

    Add new PS file(prgrms-181848.js)

---
+++ prgrms/javascript/181848.js
@@ -0,0 +1,15 @@
+/*
+문자열을 정수로 변환하기
+https://school.programmers.co.kr/learn/courses/30/lessons/181848
+ */
+
+function solution(n_str) {
+  const answer = +n_str;
+
+  return answer;
+}
+
+const input = ["10"];
+// const input = ["8542"];
+
+console.log(solution(...input));
