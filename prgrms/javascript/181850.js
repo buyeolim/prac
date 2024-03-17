c0a6533d111220e2f15eb1e129fb469b6314c014
buyeolim <buyeolim@gmail.com>
2024-03-17 22:12:22 +0900

    Add new PS file(prgrms-181850.js)

---
+++ prgrms/javascript/181850.js
@@ -0,0 +1,15 @@
+/*
+정수 부분
+https://school.programmers.co.kr/learn/courses/30/lessons/181850
+ */
+
+function solution(flo) {
+  const answer = parseInt(flo);
+
+  return answer;
+}
+
+const input = [1.42];
+// const input = [69.32];
+
+console.log(solution(...input));
