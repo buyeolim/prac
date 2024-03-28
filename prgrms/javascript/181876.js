dbd6230494849812676d2f84d0d22161d9a3cbba
buyeolim <buyeolim@gmail.com>
2024-03-28 22:59:59 +0900

    Add new PS file(prgrms-181876.js)

---
+++ prgrms/javascript/181876.js
@@ -0,0 +1,15 @@
+/*
+소문자로 바꾸기
+https://school.programmers.co.kr/learn/courses/30/lessons/181876
+ */
+
+function solution(myString) {
+  const answer = myString.toLowerCase();
+
+  return answer;
+}
+
+// const input = ["aBcDeFg"];
+const input = ["aaa"];
+
+console.log(solution(...input));
