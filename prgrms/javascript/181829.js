729c35796dbea87b82f2399a506aaf1f88318d45
buyeolim <buyeolim@gmail.com>
2024-06-05 23:13:19 +0900

    Add new PS file(prgrms-181829.js)

---
+++ prgrms/javascript/181829.js
@@ -0,0 +1,28 @@
+/*
+이차원 배열 대각선 순회하기
+https://school.programmers.co.kr/learn/courses/30/lessons/181829
+ */
+
+function solution(board, k) {
+  let answer = 0;
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      i + j <= k && (answer += board[i][j]);
+    }
+  }
+
+  return answer;
+}
+
+const input = [
+  [
+    [0, 1, 2],
+    [1, 2, 3],
+    [2, 3, 4],
+    [3, 4, 5],
+  ],
+  2,
+];
+
+console.log(solution(...input));
